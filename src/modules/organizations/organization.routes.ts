c69f788656518e4b039ddd5f59af1359648e63b5
Bogumił Sikora <bogumil.sikora@gmail.com>
2026-05-27 14:39:57 +0200

    Use validate body function in organization.routes.ts

--- src/modules/organizations/organization.routes.ts
+++ src/modules/organizations/organization.routes.ts
@@ -1,3 +1,17 @@
 import { Router } from 'express';
+import { validate } from '../../shared/validate-request.middleware';
+import { createOrganizationBodySchema } from './organization.schemas';
 
 export const organizationRoutes = Router();
+
+organizationRoutes.get('/', (_req, res) => {
+  res.status(200).json({
+    status: 'success',
+  });
+});
+
+organizationRoutes.post('/', validate({ body: createOrganizationBodySchema }), (_req, res) => {
+  return res.status(201).json({
+    organizationCreate: true,
+  });
+});
