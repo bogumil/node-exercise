3e8ad983fd1ed45edb328582f3aa761e1db1ebaf
Bogumił Sikora <bogumil.sikora@gmail.com>
2026-05-27 09:25:36 +0200

    Add mysql2 dependency.

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,6 +1,15 @@
-const env = {
+const envConfig = {
+  db: {
+    host: process.env.MYSQL_HOST || 'localhost',
+    port: parseInt(process.env.MYSQL_PORT || '3306', 10),
+    rootPassword: process.env.MYSQL_ROOT_PASSWORD,
+    database: process.env.MYSQL_DATABASE || '',
+    user: process.env.MYSQL_USER || '',
+    password: process.env.MYSQL_PASSWORD,
+    dialect: 'mysql' as const,
+  },
   envName: process.env.ENV_NAME || 'development',
   port: parseInt(process.env.PORT || '3000', 10),
 };
 
-export default env;
+export default envConfig;
