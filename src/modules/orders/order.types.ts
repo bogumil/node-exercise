e9ae34a1cc69750f60c03a19df0d92de44cb3d18
Bogumił Sikora <bogumil.sikora@gmail.com>
2026-05-28 15:45:55 +0200

    Add Order and OrderWithRelations types

---
+++ src/modules/orders/order.types.ts
@@ -0,0 +1,18 @@
+import type { Organization } from '../organizations/organization.types';
+import type { User } from '../users/user.types';
+
+export type Order = {
+  id: string;
+  organizationId: string;
+  userId: string;
+  totalAmount: number;
+  orderDate: string;
+};
+
+export type OrderWithRelations = {
+  id: string;
+  totalAmount: number;
+  orderDate: string;
+  user: User;
+  organization: Organization;
+};
