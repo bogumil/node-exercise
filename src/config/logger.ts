eea8b71e1f18f526017a3d29014097d2ab04eb79
Bogumił Sikora <bogumil.sikora@gmail.com>
2026-05-28 09:46:05 +0200

    Use pino-pretty on dev environment only

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -1,10 +1,20 @@
 import pino from 'pino';
 import envConfig from './env';
 
+const isDevelopment = envConfig.envName === 'dev';
+
 export const logger = pino({
   level: envConfig.logLevel,
   redact: ['headers.authorization', 'headers.cookie', 'headers["set-cookie"]'],
-  transport: {
-    target: 'pino-pretty',
-  },
+  ...(isDevelopment
+    ? {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            colorize: true,
+            translateTime: 'SYS:standard',
+          },
+        },
+      }
+    : {}),
 });
